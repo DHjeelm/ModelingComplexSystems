50129f83ecdeaea023bbac3bb17af5828782797b
Mannewreeby <manne.wreeby@tvinn.se>
2022-04-05 08:38:21 +0200

    parameter value updates, tried to implement 'never infected' but it does not work yet

--- lab1/spatial epidemics/3.py
+++ lab1/spatial epidemics/3.py
@@ -1,4 +1,4 @@
-from turtle import update
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from random import random
@@ -11,21 +11,28 @@ infected = 1
 recovered = 2
 immune = 3
 
-probabilities = {"initiallyInfected": .01,
-                 immune: .01, "recovery": .3, immune: .1, "infection": .1}
+timeToBecomeSusceptible = 15
+
+
+probabilities = {"initiallyInfected": .001,
+                 immune: 0.002, "recovery": .4, "infection": .7}
 
 
 def initPopulation(N: int):
+    nrInfected = 0
     population = np.zeros((N, N))
-    for i in range(N):
-        for j in range(N):
-            if random() <= probabilities["initiallyInfected"]:
-                population[i, j] = infected
+    # for i in range(N):
+    #     for j in range(N):
+    #         if random() <= probabilities["initiallyInfected"]:
+    #             population[i, j] = infected
+    #             nrInfected += 1
+    if nrInfected == 0:
+        population[N//2, N//2] = infected
 
     return population
 
 
-def updatePopulation(population: np.ndarray):
+def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
     newState = deepcopy(population)
     N = len(population)
 
@@ -38,7 +45,11 @@ def updatePopulation(population: np.ndarray):
 
             # Check if person has just recovered
             if person == recovered:
-                newState[i, j] = susceptible
+                if recoveredMask[i, j] == 0:
+                    newState[i, j] = susceptible
+                    continue
+                else:
+                    recoveredMask[i, j] -= 1
                 continue
 
             # Check if infected
@@ -47,6 +58,7 @@ def updatePopulation(population: np.ndarray):
                     newState[i, j] = immune
                 else:
                     newState[i, j] = recovered
+                    recoveredMask[i, j] = timeToBecomeSusceptible
                 continue
 
             # Susceptible
@@ -64,35 +76,49 @@ def updatePopulation(population: np.ndarray):
                 else:
                     continue
                 break
+
     return newState
 
 
-def plotPopulation(population):
+def plotPopulation(population, title: str):
+
     cmap = ListedColormap(["white", "red", "yellow", "blue"])
     susceptible_patch = mPatches.Patch(color="white", label="Susceptible")
     infected_patch = mPatches.Patch(color="red", label="Infected")
     recovered_patch = mPatches.Patch(color="yellow", label="Recovered")
     immune_patch = mPatches.Patch(color="blue", label="Immune")
+    neverInfected_patch = mPatches.Patch(color="green", label="Never Infected")
 
     plt.figure(1)
-    plt.title("Population")
+    plt.title(title)
     plt.legend(handles=[infected_patch, susceptible_patch,
-                        recovered_patch, immune_patch],bbox_to_anchor=(1.05, 1), loc=2)
+                        recovered_patch, immune_patch], bbox_to_anchor=(1.05, 1), loc=2)
     plt.imshow(population, vmin=0, vmax=len(cmap.colors), cmap=cmap)
     plt.yticks(color="w")
+
     plt.show()
 
 
 if __name__ == "__main__":
 
     N = 100
-    population = initPopulation(N)
-    plotPopulation(population)
+    recoveredMask = np.zeros((N, N))
 
-    nSteps = 150
+    population = initPopulation(N)
+    plotPopulation(population, "Initial state")
+
+    nSteps = 400
     plt.ion()
 
     for i in range(nSteps):
-        population = updatePopulation(population)
-        plotPopulation(population)
-        plt.pause(0.2)
+        population = updatePopulation(population, recoveredMask)
+        if not np.any(population == 1):
+            plt.close()
+            plt.ioff()
+            plotPopulation(population, "Cured population")
+            break
+        plt.close()
+        plotPopulation(population, f"Population {i + 1}")
+        plt.pause(0.01)
+
+    # plotPopulation(population, "Final state")
