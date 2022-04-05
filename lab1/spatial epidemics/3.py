d17175f9072e146d4e50d54e58df0a9f8e28c0c1
DHjeelm <dnl1@live.se>
2022-04-05 11:34:40 +0200

    Commented file and remove some redudant code

--- lab1/spatial epidemics/3.py
+++ lab1/spatial epidemics/3.py
@@ -6,32 +6,35 @@ from copy import deepcopy
 from matplotlib.colors import ListedColormap
 import matplotlib.patches as mPatches
 
+# State to number representation
 susceptible = 0
 infected = 1
 recovered = 2
 immune = 3
 
+# Time for a recovered person to become susceptible
 timeToBecomeSusceptible = 15
 
-
+# The built-in probabilites of the model
 probabilities = {"initiallyInfected": .001,
-                 immune: 0.002, "recovery": .4, "infection": .7}
+                 immune: 0.3, "recovery": .2, "infection": .3}
 
 
+# Function that initiliaze the population with infected individuals
 def initPopulation(N: int):
     nrInfected = 0
     population = np.zeros((N, N))
-    # for i in range(N):
-    #     for j in range(N):
-    #         if random() <= probabilities["initiallyInfected"]:
-    #             population[i, j] = infected
-    #             nrInfected += 1
+    for i in range(N):
+        for j in range(N):
+            if random() <= probabilities["initiallyInfected"]:
+                population[i, j] = infected
+                nrInfected += 1
     if nrInfected == 0:
         population[N//2, N//2] = infected
 
     return population
 
-
+# Function that updates the state of the population according to the rules 
 def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
     newState = deepcopy(population)
     N = len(population)
@@ -39,11 +42,11 @@ def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
     for i, row in enumerate(population):
         for j, person in enumerate(row):
 
-            # check if person is immune
+            # Immune rule
             if person == immune:
                 continue
 
-            # Check if person has just recovered
+            # Recovered rule
             if person == recovered:
                 if recoveredMask[i, j] == 0:
                     newState[i, j] = susceptible
@@ -52,7 +55,7 @@ def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
                     recoveredMask[i, j] -= 1
                 continue
 
-            # Check if infected
+            # Infected rule
             if person == infected and random() <= probabilities["recovery"]:
                 if random() <= probabilities[immune]:
                     newState[i, j] = immune
@@ -61,13 +64,11 @@ def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
                     recoveredMask[i, j] = timeToBecomeSusceptible
                 continue
 
-            # Susceptible
-            # check neighbors
+            # Susceptible rule
             for r in range(-1, 2):
                 for c in range(-1, 2):
                     if r == 0 and c == 0:
                         continue
-
                     rr = r + i if r + i < N else 0
                     cc = c + j if c + j < N else 0
                     if population[rr, cc] == infected and random() <= probabilities["infection"]:
@@ -79,7 +80,7 @@ def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
 
     return newState
 
-
+# Function that plots the population with colors and labels
 def plotPopulation(population, title: str):
 
     cmap = ListedColormap(["white", "red", "yellow", "blue"])
@@ -98,25 +99,39 @@ def plotPopulation(population, title: str):
 
     plt.show()
 
-
+# Simulation
 if __name__ == "__main__":
 
-    N = 100
+    # Size of board
+    N = 1000
+
+    # List that takes care of the timeToBecomeSuseptible 
     recoveredMask = np.zeros((N, N))
 
+    # Initilaize population
     population = initPopulation(N)
+
+    # Plot initial state
     plotPopulation(population, "Initial state")
 
+    # Number of simulation steps
     nSteps = 400
-    plt.ion()
 
+
+    plt.ion()
     for i in range(nSteps):
+        
+        # Update population
         population = updatePopulation(population, recoveredMask)
+
+        # Check if the epidemic is cured
         if not np.any(population == 1):
             plt.close()
             plt.ioff()
             plotPopulation(population, "Cured population")
             break
+
+        # Plot population
         plt.close()
         plotPopulation(population, f"Population {i + 1}")
         plt.pause(0.01)
