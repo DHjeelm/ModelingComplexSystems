889c187a5da99da3a842b5d1e37e460e2b9f65b0
Mannewreeby <manne.wreeby@tvinn.se>
2022-04-06 12:24:28 +0200

    Remade code with classes

--- lab1/spatial epidemics/3.py
+++ lab1/spatial epidemics/3.py
@@ -5,6 +5,7 @@ from random import random
 from copy import deepcopy
 from matplotlib.colors import ListedColormap
 import matplotlib.patches as mPatches
+from sympy import E
 
 susceptible = 0
 infected = 1
@@ -12,53 +13,91 @@ recovered = 2
 immune = 3
 
 timeToBecomeSusceptible = 15
+timeToLooseImmunity = 150
+maximumTimeToRecover = 20
 
 
-probabilities = {"initiallyInfected": .001,
-                 immune: 0.002, "recovery": .4, "infection": .7}
+probabilities = {"initiallyInfected": .01,
+                 immune: 0.3, "recovery": .1, "infection": .3}
 
 
-def initPopulation(N: int):
-    nrInfected = 0
-    population = np.zeros((N, N))
-    # for i in range(N):
-    #     for j in range(N):
-    #         if random() <= probabilities["initiallyInfected"]:
-    #             population[i, j] = infected
-    #             nrInfected += 1
-    if nrInfected == 0:
-        population[N//2, N//2] = infected
+class Person:
 
-    return population
+    def __init__(self, state=susceptible) -> None:
+        self.newState = None
+        self.state = susceptible
+        self.timeToSusceptible = 0
+        self.timeToRecover = 0
+
+    def infect(self):
+        self.oldState = self.state
+        self.state = infected
+        self.timeToRecover = random() * maximumTimeToRecover
+
+    def recover(self):
+        self.state = recovered
 
 
-def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
-    newState = deepcopy(population)
-    N = len(population)
+class Population:
+    def __init__(self, N: int) -> None:
 
-    for i, row in enumerate(population):
+        nrInfected = 0
+        self.N = N
+        self.individuals = [[Person() for _ in range(N)] for i in range(N)]
+        for i in range(N):
+            for j in range(N):
+                if random() <= probabilities["initiallyInfected"]:
+                    self.individuals[i][j].infect()
+                    nrInfected += 1
+
+        print(self.getStates())
+
+        if nrInfected == 0:
+            self.individuals[N//2][N//2].infect()
+        print(self.getStates())
+
+    def getStates(self):
+        return [[person.state for person in persons] for persons in self.individuals]
+
+    def applyNewState(self):
+        for persons in self.individuals:
+            for person in persons:
+                if person.newState is not None:
+                    person.state = person.newState
+
+
+def updatePopulation(population: Population):
+    N = population.N
+
+    for i, row in enumerate(population.individuals):
         for j, person in enumerate(row):
 
             # check if person is immune
-            if person == immune:
+            if person.state == immune or person.state == recovered:
+                if person.timeToSusceptible == 0:
+                    person.newState = susceptible
+                else:
+                    person.timeToSusceptible -= 1
+
                 continue
 
-            # Check if person has just recovered
-            if person == recovered:
-                if recoveredMask[i, j] == 0:
-                    newState[i, j] = susceptible
-                    continue
-                else:
-                    recoveredMask[i, j] -= 1
-                continue
+            # # Check if person has just recovered
+            # if person.state == recovered:
+            #     if person.timeToSusceptible == 0:
+            #         person.newState = susceptible
+            #     else:
+            #         person.timeToSusceptible -= 1
+            #     continue
 
             # Check if infected
-            if person == infected and random() <= probabilities["recovery"]:
+            if person.state == infected and random() <= probabilities["recovery"]:
                 if random() <= probabilities[immune]:
-                    newState[i, j] = immune
+                    person.newState = immune
+                    person.timeToSusceptible = timeToLooseImmunity
+
                 else:
-                    newState[i, j] = recovered
-                    recoveredMask[i, j] = timeToBecomeSusceptible
+                    person.newState = recovered
+                    person.timeToSusceptible = timeToBecomeSusceptible
                 continue
 
             # Susceptible
@@ -70,17 +109,16 @@ def updatePopulation(population: np.ndarray, recoveredMask: np.ndarray):
 
                     rr = r + i if r + i < N else 0
                     cc = c + j if c + j < N else 0
-                    if population[rr, cc] == infected and random() <= probabilities["infection"]:
-                        newState[i, j] = infected
+                    if population.individuals[rr][cc].state == infected and random() <= probabilities["infection"]:
+                        person.newState = infected
                         break
                 else:
                     continue
                 break
+    population.applyNewState()
 
-    return newState
 
-
-def plotPopulation(population, title: str):
+def plotPopulation(population: Population, title: str):
 
     cmap = ListedColormap(["white", "red", "yellow", "blue"])
     susceptible_patch = mPatches.Patch(color="white", label="Susceptible")
@@ -89,11 +127,14 @@ def plotPopulation(population, title: str):
     immune_patch = mPatches.Patch(color="blue", label="Immune")
     neverInfected_patch = mPatches.Patch(color="green", label="Never Infected")
 
+    # print(population.getStates())
+
     plt.figure(1)
     plt.title(title)
     plt.legend(handles=[infected_patch, susceptible_patch,
                         recovered_patch, immune_patch], bbox_to_anchor=(1.05, 1), loc=2)
-    plt.imshow(population, vmin=0, vmax=len(cmap.colors), cmap=cmap)
+    plt.imshow(population.getStates(),
+               vmin=0, vmax=len(cmap.colors), cmap=cmap)
     plt.yticks(color="w")
 
     plt.show()
@@ -102,20 +143,20 @@ def plotPopulation(population, title: str):
 if __name__ == "__main__":
 
     N = 100
-    recoveredMask = np.zeros((N, N))
 
-    population = initPopulation(N)
+    population = Population(N)
+    population.getStates()
     plotPopulation(population, "Initial state")
 
     nSteps = 400
     plt.ion()
 
     for i in range(nSteps):
-        population = updatePopulation(population, recoveredMask)
-        if not np.any(population == 1):
+        updatePopulation(population)
+        if not np.any(np.array(population.getStates()) == 1):
             plt.close()
             plt.ioff()
-            plotPopulation(population, "Cured population")
+            plotPopulation(population, f"Population cured after {i + 1} steps")
             break
         plt.close()
         plotPopulation(population, f"Population {i + 1}")
