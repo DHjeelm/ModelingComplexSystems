9063fd211aa76f89d4d2abd5086aa1f83a859acd
DHjeelm <dnl1@live.se>
2022-05-13 11:34:59 +0200

    Now the algorithm seems to work. Plotting the average fitness over generation, asignificant progress is displayed

--- Lab5/2/runner.py
+++ Lab5/2/runner.py
@@ -1,5 +1,4 @@
-from numpy import argmax, size
-from regex import P
+
 from painter_play import *
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -13,6 +12,7 @@ def plotChromosome(score, xpos, ypos, room):
     notPainted = mPatches.Patch(color="white", label="Not painted")
     paintedOnce = mPatches.Patch(color="green", label="Painted once")
     paintedSeveral = mPatches.Patch(color="red", label="Painted several times")
+    plt.ion()
     for i in range(len(xpos)):
         # If painter have been on position once
         
@@ -22,7 +22,7 @@ def plotChromosome(score, xpos, ypos, room):
         # Painter have been on this position more than once
         else:
             plotRoom[xpos[i]-1,ypos[i]-1] = 2
-        
+        plt.close()
         plt.figure(1, figsize=(18, 8))
         plt.legend(handles=[paintedSeveral, paintedOnce,
                             notPainted], loc="best")
@@ -33,8 +33,8 @@ def plotChromosome(score, xpos, ypos, room):
         plt.xlabel("X")
         plt.ylabel("Y")
         plt.title(f"Chromosome with score {round(score,2)}")
-
         plt.show()
+        plt.pause(0.001)
 
 def findNextGeneration(generation, fitness):
 
@@ -42,7 +42,7 @@ def findNextGeneration(generation, fitness):
     mutationRate = 0.002
 
     # Find index where to do crossover
-    crossOverCut = random.randint(0,len(generation[1]))
+    crossOverPoint = random.randint(0,len(generation[1]))
 
     # Calculate the cumalative sum of fitness and normalize it
     cumSumFitness = np.cumsum(fitness)/sum(fitness)
@@ -61,8 +61,13 @@ def findNextGeneration(generation, fitness):
 
         # Perform crossover for the new chromosome
         nextChromosome = np.zeros_like(generation[0])
-        nextChromosome[0:crossOverCut] = generation[parents[1], 0:crossOverCut]
-        nextChromosome[crossOverCut+1:-1] = generation[parents[1], crossOverCut+1:-1]
+        nextChromosome[0:crossOverPoint] = generation[parents[0], 0:crossOverPoint]
+        nextChromosome[crossOverPoint:len(nextChromosome)] = generation[parents[1], crossOverPoint:len(nextChromosome)]
+        # if i == 1:
+        #     print(f"Cross over cut: {crossOverPoint}")
+        #     print(f"Parent 1: {generation[parents[0], 0:crossOverPoint]}")
+        #     print(f" Parent 2: {generation[parents[1], crossOverPoint:len(nextChromosome)]}")
+        #     print(f"Child: {nextChromosome}")
 
         # Perform mutation
         for k in range(len(nextChromosome)):
@@ -76,11 +81,19 @@ def findNextGeneration(generation, fitness):
 
 if __name__ == "__main__":
 
-    # # Initialize room and rules
+    # Initialize room and rules
     # test_room=np.zeros((20,40))
     # test_rules=np.ones((54,1))
     # for i in range(len(test_rules)):
     #     test_rules[i]=3
+ 
+    # # Run painter
+    # score, xpos, ypos = painter_play(test_rules, test_room)
+    # # Plot painter
+    # plotChromosome(score, xpos, ypos, test_room)
+
+    
+    
 
     # Create a empty room of size (20 x 40)
     room=np.zeros((20,40))
@@ -92,40 +105,60 @@ if __name__ == "__main__":
 
     # Settings
     numberOfTimesToRunRules = 10
-    numberOfGenerations = 100
+    numberOfGenerations = 200
 
     # The last generation and its fitness
-    lastFitness = []
-    lastGeneration = np.zeros((numberOfChromosomes, lengthOfChromosomes))
+    # lastFitness = []
+    # lastGeneration = np.zeros((numberOfChromosomes, lengthOfChromosomes))
 
     generation = firstGeneration
+    averageFitness = []
+
+    # Loop through generation
     for i in range(numberOfGenerations):
-        print(f"Generation {i}")
         # Create list to save fitness
         fitness = []
+        # Loop through each chromosome of this generation
         for j in range(numberOfChromosomes):
-            innerFitness = 0
+            fitnessChromosome = 0
+            # Run each chromose numberOfTimesToRunRules times
             for k in range(numberOfTimesToRunRules):
-                score, xpos, ypos = painter_play(firstGeneration[i], room)
-                innerFitness+=score
-            
-            fitness.append(innerFitness/numberOfTimesToRunRules)
-        print(np.max(fitness))
-        # Save previous generation
+                score, xpos, ypos = painter_play(generation[j], room)
+                fitnessChromosome+=score
+            # Save average fitness of the chromose
+            fitness.append(fitnessChromosome/numberOfTimesToRunRules)
+        
+        # Save average fitness for this generation
+        averageFitness.append(np.mean(fitness))
+
+        # Save this generation
         prevGeneration = generation
+
+        # Find next generation
         generation = findNextGeneration(generation, fitness)
+
+        # Save last generation
         if i == numberOfGenerations-1:
             maxFitness = np.argmax(fitness)
             bestChromosome = prevGeneration[maxFitness]
             lastFitness = fitness
             lastGeneration = generation
+        print(f"Generation {i} has average fitness: {np.round(np.mean(fitness),4)} ")
 
-    # print(bestChromosome)
-    # print(lastFitness)
+    # Plot average fitness vs generation
+    plt.figure(5)
+    plt.plot(list(range(0,numberOfGenerations)), averageFitness)
+    plt.xlabel("Generation")
+    plt.ylabel("Average fitness")
+    plt.title("Average fitness vs generation")
 
+    plt.show()
+   
     # Run painter
     score, xpos, ypos = painter_play(bestChromosome, room)
     # Plot painter
+    plt.plot([1,2,3],[1,2,3])
+    plt.show()
     plotChromosome(score, xpos, ypos, room)
 
 
