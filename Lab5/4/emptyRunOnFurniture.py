d3b3c79ef0075e695aedf7536f3b0870827a1f35
DHjeelm <dnl1@live.se>
2022-05-13 15:22:03 +0200

    Added script for running the best chromosome from 2 on a room with furniture. Added plot of its trajectory as well

--- Lab5/4/emptyRunOnFurniture.py
+++ Lab5/4/emptyRunOnFurniture.py
@@ -1,4 +1,4 @@
-from painter_play import *
+from painter_play_walls import *
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import matplotlib.patches as mPatches
@@ -6,27 +6,31 @@ import random
 
 def plotChromosome(score, xpos, ypos, room):
     plotRoom = room
+    # print(room)
 
-    cmap = ListedColormap(["white", "green", "red"])
+    cmap = ListedColormap(["white", "blue", "green", "red"])
     notPainted = mPatches.Patch(color="white", label="Not painted")
-    paintedOnce = mPatches.Patch(color="green", label="Painted once")
+    paintedOnce = mPatches.Patch(color="green", label="Painted")
     paintedSeveral = mPatches.Patch(color="red", label="Painted several times")
+    wall = mPatches.Patch(color="blue", label="Wall")
+
     plt.ion()
     for i in range(len(xpos)):
         # If painter have been on position once
-        
-        if plotRoom[xpos[i]-1,ypos[i]-1] == 0:
-            plotRoom[xpos[i]-1,ypos[i]-1] = 1
+        if plotRoom[xpos[i],ypos[i]] == 0:
+            plotRoom[xpos[i],ypos[i]] = 2
 
         # Painter have been on this position more than once
         else:
-            plotRoom[xpos[i]-1,ypos[i]-1] = 2
+            plotRoom[xpos[i],ypos[i]] = 3
+        
+        if i == 1:
+            print(plotRoom)
         plt.close()
         plt.figure(1, figsize=(18, 8))
-        plt.legend(handles=[paintedSeveral, paintedOnce,
-                            notPainted], loc="best")
+        plt.legend(handles=[paintedOnce, wall, notPainted, paintedSeveral], loc="best")
         plt.imshow(plotRoom, vmin=0, vmax=len(cmap.colors), cmap=cmap)
-        plt.text(ypos[i]-1, xpos[i]-1, str("Painter"), va='center', ha='center', size=5)
+        plt.text(ypos[i], xpos[i], str("Painter"), va='center', ha='center', size=5)
         # plt.imshow(plotRoom)
         plt.yticks(color="w")
         plt.xlabel("X")
@@ -38,10 +42,19 @@ def plotChromosome(score, xpos, ypos, room):
 
 if __name__ == "__main__":
 
+    # Best chromosome on 
     bestChromosome = [1, 1, 0, 3, 1, 1, 0, 2, 1, 1, 2, 2, 0, 0, 1, 3, 2, 2, 2, 2, 2, 1, 3, 1, 2, 2, 2, 0, 2, 2, 2, 3, 2, 3, 0, 2, 2, 2, 3, 3, 2, 1, 3, 2, 1, 0, 3, 3, 2, 2, 2, 1, 3, 1]
     room=np.zeros((20,40))
-    score, xpos, ypos = painter_play(bestChromosome, room)
-    plt.plot([1,2,3],[1,2,3])
-    plt.show()
-    plotChromosome(score, xpos, ypos, room)
+    indices = [(m, n) for m in range(20) for n in range(40)]
+    randomIndices = random.sample(indices, 100)
+    for idx in randomIndices:
+        room[idx[0], idx[1]] = 1
+    # for i in range(100):
+    # room[]
+        
+    score, xpos, ypos, env, plotEnv = painter_play(bestChromosome, room, randomIndices)
+    # plt.plot([1,2,3],[1,2,3])
+    # plt.show()
+    print(plotEnv)
+    plotChromosome(score, xpos, ypos, plotEnv)
 
